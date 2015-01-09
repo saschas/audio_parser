89a7c879225864050ec80bafa3093aa757ff9f01
Sascha Sigl <saschasigl@gmx.de>
2015-01-09 16:57:58 +0100

    audio_parser is ready to parse the audio Data
    Signed-off-by: Sascha Sigl <saschasigl@gmx.de>

--- js/index.js
+++ js/index.js
@@ -38,26 +38,12 @@
             $time = 0;
         };
 
-    // Create the analyser
-    var analyser = context.createAnalyser();
-        analyser.fftSize = 64;
-    var frequencyData = new Uint8Array(analyser.frequencyBinCount);
-  
-  
-    // Set up the visualisation elements
-   
-    var barSpacingPercent = 100 / analyser.frequencyBinCount;
-  
-    var $array = [];
-    for (var i = 0; i < analyser.frequencyBinCount; i++) {
-      $array.push(i * barSpacingPercent);
-    }
-  
-    // Get the frequency data and update the visualisation
-  
-    var $audioData = [];
+    
     var $loop;
 
+//////////////////////////////////////////
+    //   Three.js
+//////////////////////////////////////////
     var stats = new Stats();
     stats.setMode(0); // 0: fps, 1: ms
 
@@ -70,9 +56,15 @@
   
     // revolutions per second
     var scene = new THREE.Scene();
+//////////////////////////////////////////
+    //   Camera
+//////////////////////////////////////////
     var camera = new THREE.PerspectiveCamera( 35, window.innerWidth/window.innerHeight, 0.5, 1000 );
         camera.position.y = 1;
         camera.position.z = 5.5;
+//////////////////////////////////////////
+    //   Renderer
+//////////////////////////////////////////
     var renderer = new THREE.WebGLRenderer({ antialias: true });
         renderer.setSize( window.innerWidth, window.innerHeight );
         renderer.setClearColor( 0xcccccc, 0 );
@@ -85,9 +77,22 @@
         renderer.shadowMapBias = 0.1;
         renderer.shadowMapDarkness = 0.5;
 
+//////////////////////////////////////////
+    //   Controls
+//////////////////////////////////////////
     var controls = new THREE.OrbitControls( camera );
         controls.rotateSpeed = 0.1;
         controls.keyPanSpeed = 0.001;
+
+
+//////////////////////////////////////////
+    //   Resize
+//////////////////////////////////////////
+window.onresize = function(){
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
 /////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////// Plane
 /////////////////////////////////////////////////////////////////////////////////////////////////
@@ -103,18 +108,18 @@
 //////////////////////////////// Light
 /////////////////////////////////////////////////////////////////////////////////////////////////
      var spotLight = new THREE.SpotLight( 0xffffff,1 );
-         spotLight.position.set( 5,3, 10 );
-   /*       
+         spotLight.position.set( 5,40, 10 );
+          
          spotLight.castShadow = true;
           
          spotLight.target.position.set(0, 0, 0 );
          spotLight.shadowDarkness = 0.5;
           
-          spotLight.shadowMapWidth = 4096; // default is 512
-          spotLight.shadowMapHeight = 4096; // default is 512
+          spotLight.shadowMapWidth = 2048; // default is 512
+          spotLight.shadowMapHeight = 2048; // default is 512
          spotLight.shadowCameraNear = 1; 
          spotLight.shadowCameraFar = 50;
- */         
+          
         scene.add( spotLight );
 
 
@@ -128,41 +133,59 @@
 
     
 
+
 /////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// Audio Data
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+var $_AUDIO_STREAM = {};
+$.getJSON( "js/audioData.json", function( audio_data ) {
+  $_AUDIO_STREAM.data = audio_data;
+  //$loop = render();
+  // init();
+});
+
+
 //////////////////////////////// Audio
 /////////////////////////////////////////////////////////////////////////////////////////////////
 var group;
 var currCount = 0;
 var $max = 10; 
 var onScreen = [];
-var $duration = $("#player").get(0).duration;
+
+var $player = $('#player');
+
+var $duration = $player.get(0).duration;
 
 var $timeLineWidth = $('#audioNav').width()-100;
 
-var source = context.createMediaElementSource($("#player").get(0));
-    source.connect(analyser);
-    analyser.connect(context.destination);
 
-  $("#player").on({
+
+
+  $player.on({
       canplay : function() {
         
       },
       timeupdate: function(event){
         
-        analyser.getByteFrequencyData(frequencyData);
-        var $currentTime = Math.round($(this).get(0).currentTime * 100 / $timeLineWidth );
-        var $x = $currentTime*0.01*$timeLineWidth;
-        $('.time').css({width: $currentTime});
+        // collectAudioData.tick();
+        // 
 
+        displayAudioData();
       },
       play: function(){
         $loop = render();
-        init();
-        $("#player").get(0).muted = true;
+        console.log('play');
+
+          $player.get(0).playbackRate = 1;
+        init();        
       },
       pause : function(){
           $('.output').empty();
-          cancelAnimationFrame($loop);
+          //cancelAnimationFrame($loop);
+      },
+      ended : function(){
+        collectAudioData.audioDataInConsole();
       }
   })
   $('#audioNav').bind({
@@ -172,86 +195,145 @@ var source = context.createMediaElementSource($("#player").get(0));
         
         $('.time').css('width', $x);
 
-        $("#player").get(0).currentTime = $duration * $currentTime/100;
+        $player.get(0).currentTime = $duration * $currentTime/100;
       }
   });
 
 
   $("#audioNav .play").bind({
     click : function(){
-      if($("#player").get(0).paused){
+      if($player.get(0).paused){
         $(this).removeClass('off').addClass('on');
-        $("#player").get(0).play();
+        $player.get(0).play();
       }
       else{
         $(this).removeClass('on').addClass('off');
-        $("#player").get(0).pause();
+        $player.get(0).pause();
         $('.output').empty();
-          cancelAnimationFrame($loop);
+          // cancelAnimationFrame($loop);
       }
       
     }
   });
   $("#audioNav .pause").bind({
     click : function(){
-      $("#player").get(0).pause();
+      $player.get(0).pause();
     }
   });
+
+
+  //Each timeupdate
+  function collectAudioData(){
+      var analyser,
+          frequencyData,
+          source;
+      var $audioStream = [];
+      this.init = function(){
+        // Create the analyser
+      var analyser = context.createAnalyser();
+          analyser.fftSize = 64;
+  
+      // Get the frequency data and
+      var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+  
+      // Set source
+      var source = context.createMediaElementSource($player.get(0));
+          source.connect(analyser);
+          analyser.connect(context.destination);
+    
+      // store it in the audioStream Object for later use
+      };
+      //timeupdate
+      this.tick = function(){
+        console.log(this.init);
+        if(!this.init){
+          this.init;
+        }
+        // get audio Data
+        analyser.getByteFrequencyData(frequencyData);
+  
+        // store audio Data in $audioStream Object for later use
+        $audioStream.push(frequencyData);
+  
+        // look at http://bgrins.github.io/devtools-snippets/#console-save for saving the object
+        // console.save(temp1,audioData.json)
+      }
+      this.audioDataInConsole = function(){
+          console.log($audioStream);
+      }
+    };
+
 
 
   var group = new THREE.Object3D();
   var $groups = [];
   var oldData;
-  
+
+
   function init(x){
-    
-    $.each(frequencyData,function(j){
 
-      $.each(frequencyData,function(i){
-        var z = -j*.5;
-        var x = i*.5  + .5;
-        if(frequencyData[i] === 0){
-          group.add(cube(x, .01 ,z));
-        }
-        else{
-          group.add(cube(x, frequencyData[i] ,z));
-        }
-
-      });
-      scene.add(group);
-    
-     }); 
-
-    
+    for (var t in $_AUDIO_STREAM.data) {
+          
+      var z = -t;
+      if(t<30){
+        for (var i in $_AUDIO_STREAM.data[t]) {
+          var x = i;
+          var y = $_AUDIO_STREAM.data[t][i];
+          group.add(cube(x, y ,z));  
+        } 
+      }
+       
+    }
+    scene.add(group);
   }
   var $data = [];
-  function anim(){
+
+
+  function displayAudioData(){
+    group.position.z +=.1;
+
+
+    var $currentTime = Math.round($(this).get(0).currentTime * 100 / $timeLineWidth );
+    var $x = $currentTime*0.01*$timeLineWidth;
+        $('.time').css({width: $currentTime});
+    
+
+    // console.log('displayAudioData');
+  }
+
+  function animation(){
       //For each group - row
-      //console.log('tick')
+      //console.log('tick');
+
+      
+    for (var t in $_AUDIO_STREAM.data){
+       //console.log(group);
+      //group.children[t].position.z = -10.5
+    }
+    /*
     if(group.children.length>0){
       var j = 0;
-      $.each(group.children,function(i){
-        
-        if(j<frequencyData.length-1){
-          j++;
-        }
-        else{
-          
-          j = 0;
-        }
 
-        if(this.position.z > 5){
-          this.position.z = -10.5;
+        for(var i=0;i<1024;i++){
+          if(j<frequencyData.length-1){
+            j++;
+          }
+          else{
+            
+            j = 0;
+          }
+          group.children[i]
+          if(group.children[i].position.z > 5){
+            group.children[i].position.z = -10.5;
 
+          }
+          else{
+            group.children[i].position.z += .1;
+          }
+          group.children[i].scale.y = frequencyData[j] * .2;
         }
-        else{
-          this.position.z += .1;
-        }
-        this.scale.y = frequencyData[j] * .2;
-
-      });
     }
-
+    */
     controls.update();
   }
   
@@ -278,7 +360,7 @@ var source = context.createMediaElementSource($("#player").get(0));
     stats.begin();
     $loop = requestAnimationFrame( render );    
     stats.end();
-    anim();
+    animation();
     renderer.render(scene, camera);
     return $loop;
   };
